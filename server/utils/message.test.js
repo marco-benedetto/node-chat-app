577f0ce21328744274cc938349bbeb0318fea7b7
marco-benedetto <marco.benedetto93@gmail.com>
2018-09-04 09:36:12 +0200

    Add geolocation support via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate the correct message object', () => {
@@ -12,6 +12,22 @@ describe('generateMessage', () => {
         expect(message).toMatchObject({
             from,
             text
+        });
+    });
+});
+
+describe('generateLocation', () => {
+    it('should generate the correct location object', () => {
+        var from = 'Tester';
+        var lat = 32;
+        var long = -3;
+        var locationMessage = generateLocationMessage(from, lat, long);
+
+        expect(typeof locationMessage.createdAt).toBe('number');
+        expect(typeof locationMessage.url).toBe('string');
+        expect(locationMessage).toMatchObject({
+            from,
+            url: `https://www.google.com/maps?q=${lat},${long}`
         });
     });
 });
